ea2a8ab7c22e8e33f6ddcead4d66bf5e1f43a265
Sahil Dubey <143827286+sahildubey11@users.noreply.github.com>
2025-12-02 23:31:35 +0530

    Enhance key press event to show modifier keys
    Added display of modifier keys (Ctrl, Alt, Shift) in h3 element.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let h1=document.querySelector("h1");
+let h3= document.querySelector("h3");
 
 let but=document.querySelector("button");
 
 window.addEventListener("keydown",function(det){
     // console.log(det); //to show you the detail
-    console.log(det.key); //to show which key is pressed
+    // console.log(det.key); //to show which key is pressed
     if (det.key === " "){
         h1.textContent= "Space";
     }
@@ -12,8 +13,9 @@ window.addEventListener("keydown",function(det){
         
         h1.textContent=det.key;
     }
+    h3.innerHTML=`Ctrl: <b>${det.ctrlKey}</b> | Alt: <b>${det.altKey}</b> | Shift: <b>${det.shiftKey}</b>`;
+});
 
-})
 
 but.addEventListener("click",function(){
     h1.textContent="Start Typing";
